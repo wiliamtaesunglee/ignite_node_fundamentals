900b26252f020cf385f0820405503eba490faf2d
guts <wiliam.tae@gmail.com>
2021-04-06 19:08:45 -0300

    feat: add get statement by cpf

--- src/index.js
+++ src/index.js
@@ -8,24 +8,30 @@ app.use(express.json());
 const customers = [];
 
 app.post('/account', (request, response) => {
-  const { cpf, name } = request.body;
+  const { cpf, name, statement } = request.body;
 
   const customerAlreadyExist = customers.some(
     (customer) => customer.cpf === cpf
   );
 
-  if (customerAlreadyExist) {
-    return response.status(400).json({ error: "Customer already exists!" });
-  }
+  if (customerAlreadyExist) return response.status(400).json({ error: "Customer already exists!" });
 
   customers.push({
     cpf,
     name,
     id: uuidv4(),
-    statement: []
+    statement,
   });
 
   return response.status(201).send();
 });
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) return response.status(400).json({ error: "Customer not found" });
+  const { statement } = customer;
+  return response.json(statement)
+})
 
 app.listen(3333);
