e3c214f1e65ca2bceaf01802cc984b008ac35bb0
guts <wiliam.tae@gmail.com>
2021-04-09 08:40:42 -0300

    feat: delete account

--- src/index.js
+++ src/index.js
@@ -95,4 +95,10 @@ app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
   return response.json(customer);
 });
+
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  customers.splice(customer, 1);
+  return response.status(200).json(customers);
+})
 app.listen(3333);
